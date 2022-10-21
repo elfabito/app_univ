cafefa9fef75de2649818d830f7d83286596d161
Fab <info@desane.com.uy>
2022-10-21 17:19:48 -0300

    Students index

---
+++ app/models/students.rb
@@ -0,0 +1,5 @@
+class Students < ApplicationRecord
+before_save { self.email = email.downcase }
+validates :name, presence: true, length: { minimum: 5, maximum:50 }
+validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 105}, format: { with: URI::MailTo::EMAIL_REGEXP }
+end
