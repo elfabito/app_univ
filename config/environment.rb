2d8441e403335e309138332d9a67986c52bac119
Fab <info@desane.com.uy>
2022-10-22 19:17:56 -0300

    Deploy to heroku

--- config/environment.rb
+++ config/environment.rb
@@ -6,4 +6,11 @@ Rails.application.initialize!
 
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   html_tag.html_safe
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+if ENV["RAILS_LOG_TO_STDOUT"].present?
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+end
 end
